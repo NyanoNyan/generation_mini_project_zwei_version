6fe47898ddcc932e69496fabe6881dd5e8715528
nyano <nayan100@hotmail.co.uk>
2021-07-17 08:42:20 +0100

    Took out append_data modules as it's not needed. Added in print statement that 0 now saves the product and courier changes. Added in data_storage and filename in the product_menu and courier_menu function as the main function now loads the data and filename and feeds it in each menu. Added in write_orders to make changs for the status changes in the orders menu. In making changes to the order, made sure to put the courier value is converted to an int.

--- helper_modules/menu_operations.py
+++ helper_modules/menu_operations.py
@@ -1,11 +1,11 @@
 from helper_modules.operations_funcs import selection_print, read_data, view_data, confirmation_prints, show_with_index, update_data, delete_data, extra_order_info
-from helper_modules.file_handler import read_data, append_data
+from helper_modules.file_handler import read_data
 from helper_modules.orders import read_orders, write_orders, status
 
 def main_menu():
 
     print('\n')
-    print('Press 0 to go to exit the application')
+    print('Press 0 to exit the application and save the data changes')
     print('Press 1 to go to product menu')
     print('Press 2 to go to couriers menu')
     print('Press 3 to go to orders menu')
@@ -30,11 +30,9 @@ def main_menu():
     
     return first_option, selection
 
-def product_menu(selection):
+def product_menu(selection, data_storage, filename):
     selection_print(selection)
-    filename = 'product.txt'
     while True:
-        data_storage = read_data(filename)
         product_menu_selection = input('Please select an option or press m to see the menu options: ')
         
         if product_menu_selection == "0":
@@ -42,28 +40,28 @@ def product_menu(selection):
         elif product_menu_selection == "1":
             view_data(selection, data_storage)
         elif product_menu_selection == "2":
-            new_input = input('Please add in the a new product name: ')    
-            append_data(new_input, filename)
+            new_input = input('Please add in the a new product name: ')   
+            data_storage.append(new_input) 
             confirmation_prints(new_input)
         elif product_menu_selection == "3":
             show_with_index(selection, filename)
             previous_input_index = input(f'Please insert the {selection} index you want to change: ')
             new_name = input('Please insert the new product name:')
-            update_data(previous_input_index, new_name, filename, data_storage, selection)
+            data_storage = update_data(previous_input_index, new_name, data_storage, selection)
         elif product_menu_selection == "4":
             show_with_index(selection, filename)
             delete_input = input(f'Please insert the index of the {selection} you want deleted: ')
-            delete_data(delete_input, filename, data_storage)
+            data_storage = delete_data(delete_input, data_storage)
         elif product_menu_selection == "m":
             selection_print(selection)
         else:
             print('Please enter a valid input')
-            
-def courier_menu(selection):
+        
+    return data_storage
+    
+def courier_menu(selection, data_storage, filename):
     selection_print(selection)
-    filename = 'courier.txt'
     while True:
-        data_storage = read_data(filename)
         courier_menu_selection = input('Please select an option or press m to see the menu options: ')
         
         if courier_menu_selection == "0":
@@ -72,21 +70,23 @@ def courier_menu(selection):
             view_data(selection, data_storage)
         elif courier_menu_selection == "2":
             new_input = input('Please add in the a new courier name: ')    
-            append_data(new_input, filename)
+            data_storage.append(new_input) 
             confirmation_prints(new_input)
         elif courier_menu_selection == "3":
             show_with_index(selection, filename)
             previous_input_index = input(f'Please insert the {selection} index you want to change: ')
             new_name = input(f'Please insert the new {selection} name:')
-            update_data(previous_input_index, new_name, filename, data_storage, selection)
+            data_storage = update_data(previous_input_index, new_name, data_storage, selection)
         elif courier_menu_selection == "4":
             show_with_index(selection, filename)
             delete_input = input(f'Please insert the index of the {selection} you want deleted: ')
-            delete_data(delete_input, filename, data_storage)
+            data_storage = delete_data(delete_input, data_storage)
         elif courier_menu_selection == "m":
             selection_print(selection)
         else:
             print('Please enter a valid input')
+        
+    return data_storage
 
 def orders_menu(selection):
     selection_print(selection)
@@ -117,7 +117,8 @@ def orders_menu(selection):
                 previous_input_index = int(input(f'Please insert the {selection} status index you want to change: '))
                 [print(index, status_name) for index, status_name in enumerate(status)]
                 new_status_index = int(input('Please insert the new order status index:'))
-                update_data(previous_input_index, new_status_index, filename, data_storage, selection)
+                data_storage = update_data(previous_input_index, new_status_index, data_storage, selection)
+                write_orders(data_storage, filename)
             except Exception as e:
                 print('\n')
                 print(f'Error: {e}')
@@ -131,7 +132,11 @@ def orders_menu(selection):
                 selected_order = data_storage[previous_input_index]
                 new_obj = {}
                 for key, value in selected_order.items():
-                    new_input = input(f'New value for {key}: ')
+                    if key == 'courier':
+                        new_input = int(input(f'New value for {key}: '))
+                    else:
+                        new_input = input(f'New value for {key}: ')
+                    
                     if new_input == "":
                         new_obj[key] = value
                     else:
