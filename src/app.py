874a36e9b51a5032cc04f217431eab181d29c5d9
Nyano <nayan100@hotmail.co.uk>
2021-07-22 15:15:19 +0100

    Added clear screen to make the menu neater

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
+import os
+
 from helper_modules.orders import write_orders
 from helper_modules.menu_operations import courier_menu, main_menu, product_menu, courier_menu, orders_menu
 from helper_modules.csv_handler import read_csv, write_csv
 
+def clear_screen():
+    os.system('clear')
+
 def app():
+    clear_screen()
     filename_product = 'product.csv'
     filename_courier = 'courier.csv'
     filename_order = 'orders.csv'
@@ -12,6 +18,7 @@ def app():
     data_order = read_csv(filename_order)
     
     while True:
+        clear_screen()
         main_menu_option, selection = main_menu()
         if main_menu_option == "0":
             print("\n")
@@ -24,10 +31,16 @@ def app():
             
             break
         elif main_menu_option == "1":
+            clear_screen()
             data_product = product_menu(selection, data_product, filename_product)
+
         elif main_menu_option == "2":
+            clear_screen()
+
             data_courier = courier_menu(selection, data_courier, filename_courier)
         elif main_menu_option == "3":
+            clear_screen()
+
             data_order = orders_menu(selection, data_order, data_product, data_courier)
         else: 
             print('Please enter a valid option')
