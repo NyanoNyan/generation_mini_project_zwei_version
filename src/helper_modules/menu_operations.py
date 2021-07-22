74e4bed526bcc9223dc48a1290a3558a255924c2
Nyano <nayan100@hotmail.co.uk>
2021-07-22 21:11:56 +0100

    Replaced some input prompts with input_helper

--- src/helper_modules/menu_operations.py
+++ src/helper_modules/menu_operations.py
@@ -1,4 +1,4 @@
-from helper_modules.operations_funcs import selection_print, read_data, view_data, confirmation_prints, show_with_index, update_data, delete_data, extra_order_info, append_data, update_dict_data
+from helper_modules.operations_funcs import selection_print, read_data, view_data, confirmation_prints, show_with_index, update_data, delete_data, extra_order_info, append_data, update_dict_data, input_helper
 from helper_modules.file_handler import read_data
 from helper_modules.orders import read_orders, write_orders, status
 from helper_modules.csv_handler import write_csv
@@ -12,15 +12,10 @@ def main_menu():
     print('Press 2 to go to couriers menu')
     print('Press 3 to go to orders menu')
     print('\n')
-    
-    while True:
-        first_option = input('Please select an option: ')
-        
-        if first_option == "1" or first_option == "2" or first_option == "3" or first_option == "0":
-            break
-        else: 
-            print('Please select a valid option!')
-    
+
+    prompt = 'Please select an option: '
+    first_option = input_helper(prompt, ['0','1', '2', '3'], False, True)
+
     if first_option == "1":
         selection = "product"
     elif first_option == "2":
@@ -125,18 +120,19 @@ def orders_menu(selection, data_storage, data_storage_product, data_storage_cour
         elif orders_menu_selection == "3":
             show_with_index(selection, data_storage)
             print('\n')
-            try: 
-                previous_input_index = int(input(f'Please insert the {selection} status index you want to change: '))
-                [print(index, status_name) for index, status_name in enumerate(status)]
-                new_status_index = int(input('Please insert the new order status index:'))
-                data_storage = update_data(previous_input_index, new_status_index, data_storage, selection)
-                
-            except Exception as e:
-                print('\n')
-                print(f'Error: {e}')
-                print('Please make sure to enter a valid option')
-                print('\n')
-                
+            # Get the status Order index
+            prompt_msg = f'Please insert the {selection} status index you want to change: '
+            condition_list = list(range(len(data_storage)))
+            previous_input_index = input_helper(prompt_msg, condition_list, True, True)
+            
+            [print(index, status_name) for index, status_name in enumerate(status)]
+            # Get the Status index
+            prompt_msg2 = f'Please insert the {selection} status index you want to change: '
+            condition_list2 = list(range(len(status)))
+            new_status_index = input_helper(prompt_msg2, condition_list2, True, True)
+            
+            data_storage = update_data(previous_input_index, new_status_index, data_storage, selection)
+
         elif orders_menu_selection == "4":
             show_with_index(selection, data_storage)
             data_storage = update_dict_data(data_storage)
@@ -144,17 +140,15 @@ def orders_menu(selection, data_storage, data_storage_product, data_storage_cour
         elif orders_menu_selection == "5":
             show_with_index(selection, data_storage)
             print('\n')
-            try:
-                index_delete = int(input('Please enter the index of order which you want to delete: '))
-                deleted = data_storage.pop(index_delete)
-                print('\n')
-                print(f'{deleted} has been deleted')
-                print('\n')
-            except Exception as e:
-                print('\n')
-                print(f'Error: {e}')
-                print('Please make sure to enter a valid option')
-                print('\n')
+            prompt_msg3 = "Please enter the index of order which you want to delete: '"
+            condition_list3 = list(range(len(data_storage)))
+            index_delete = input_helper(prompt_msg3, condition_list3, True, True)
+            deleted = data_storage.pop(index_delete)
+
+            print('\n')
+            print(f'{deleted} has been deleted')
+            print('\n')
+
         elif orders_menu_selection == "m":
             selection_print(selection)
         else:
