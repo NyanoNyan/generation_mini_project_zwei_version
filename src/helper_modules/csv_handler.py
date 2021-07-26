c18d7b4bd730d101c96da4944bdd31b940e9210f
nyano <nayan100@hotmail.co.uk>
2021-07-26 08:16:26 +0100

    Added an error message to check if the data is not in the correct format for write_csv. Added try except for read_csv and if the filepahtway is wrong an error should display and return an empty list. For convert_to_correct_type, added in the try and except block in case the format or filename given is incorrect.

--- src/helper_modules/csv_handler.py
+++ src/helper_modules/csv_handler.py
@@ -13,45 +13,62 @@ def load_field_names(name):
     return list_of_filednames.get(name)
 
 def write_csv(data, filename):
-    pathway = set_up_path(filename)
-    # name without extension
-    short_filename = filename.split('.')[0]
-    fieldnames = load_field_names(short_filename)
-    print('pathway', pathway)
-    with open(pathway, mode='w', newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=",")
-        
-        writer.writeheader()
-        for each_data in data:
-            print('testing', each_data)
-            writer.writerow(each_data)
+    error_msg = 'The data is not formated correctly'
+    if isinstance(data, list):
+        pathway = set_up_path(filename)
+        # name without extension
+        short_filename = filename.split('.')[0]
+        fieldnames = load_field_names(short_filename)
+        print('pathway', pathway)
+        with open(pathway, mode='w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=",")
+            
+            writer.writeheader()
+            for each_data in data:
+                if isinstance(each_data, dict):
+                    print('testing', each_data)
+                    writer.writerow(each_data)
+                else:
+                    print(error_msg)
+                    return error_msg
+
+    else:
+        print(error_msg)
+        return error_msg
 
 def read_csv(filename):
     pathway = set_up_path(filename)
     short_filename = filename.split('.')[0]
     store = []
-    with open(pathway, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=",")
-        
-        for row in csv_reader:
-            store.append(row)
-    
-    return convert_to_correct_type(store, short_filename)
+    try:
+        with open(pathway, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=",")
+            
+            for row in csv_reader:
+                store.append(row)
+        return convert_to_correct_type(store, short_filename)
+    except FileNotFoundError as error:
+        print(error.args[1])
+        return []
+
 
 def convert_to_correct_type(data_storage, filename):
-    if filename == 'product':
-        for data in data_storage:
-            for key, value in data.items():
-                if key == 'price':
-                    data[key] = float(value)
-        return data_storage
-    elif filename == 'orders':
-        for data in data_storage:
-            for key, value in data.items():
-                if key == 'courier':
-                    data[key] = int(value)
-                if key == 'items':
-                    data[key] = json.loads(value)
-        return data_storage
-    else:
-        return data_storage
+    try:
+        if filename == 'product':
+            for data in data_storage:
+                for key, value in data.items():
+                    if key == 'price':
+                        data[key] = float(value)
+            return data_storage
+        elif filename == 'orders':
+            for data in data_storage:
+                for key, value in data.items():
+                    if key == 'courier':
+                        data[key] = int(value)
+                    if key == 'items':
+                        data[key] = json.loads(value)
+            return data_storage
+        else:
+            return data_storage
+    except Exception as error:
+        return error
