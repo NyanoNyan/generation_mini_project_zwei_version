2faa5d76457e700121a22cd2e6f646244cc7cedc
nyano <nayan100@hotmail.co.uk>
2021-07-28 20:29:06 +0100

    Testing operation functions, testing append Initial commit

---
+++ src/tests/test_operations_funs.py
@@ -0,0 +1,12 @@
+from unittest.mock import patch
+from unittest import mock
+
+from helper_modules.operations_funcs import append_data
+
+@patch('builtins.input')
+@patch('builtins.input')
+def test_append_data(mock_input, mock_input2):
+    mock_input.return_value = 'time'
+    mock_input2.return_value = 1.4
+    
+    
