8711fe0399b352a1012744a1a342b91ee13ad1d8
nyano <nayan100@hotmail.co.uk>
2021-07-28 20:28:04 +0100

    Initial commit of testing menu_operations_funcs

---
+++ src/tests/test_menu_operations_funcs.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch
+
+from helper_modules.menu_operations import main_menu, product_menu
+
+@patch('builtins.input')
+def test_main_menu(mock_input):
+    # Product selection
+    mock_input.return_value = '1'
+    expected = ['1', 'product']
+    actual1, actual2 = main_menu()
+
+    assert actual1 == expected[0]
+    assert actual2 == expected[1]
+
+    # Courier selection
+    mock_input.return_value = '2'
+    expected = ['2', 'courier']
+    actual1, actual2 = main_menu()
+
+    assert actual1 == expected[0]
+    assert actual2 == expected[1]
+
+    # Orders selection
+    mock_input.return_value = '3'
+    expected = ['3', 'orders']
+    actual1, actual2 = main_menu()
+
+    assert actual1 == expected[0]
+    assert actual2 == expected[1]
+
+    # None selection
+
+@patch('builtins.input')
+def test_product_menu(mock_input):
+    
+    # Product selection
+    mock_input.return_value = "1"
+    pass    
+    
