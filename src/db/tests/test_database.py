d846244996a9d8c4fdb592c78474a2c20ccbaea1
Nyano <nayan100@hotmail.co.uk>
2021-08-02 19:59:42 +0100

    Setup database not needed at the moment, removed it from CRUD operations to test. For test_add_to_db, added in tests for adding data to product, courier and orders. Also changed layout for test_show_db_data, to test for dictionary format now, this is used to display.

--- src/db/tests/test_database.py
+++ src/db/tests/test_database.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, terminal_size
 import pytest
 
 from db.setup_db import HelperDB, show_db_data, add_to_db, update_to_db, delete_to_db
@@ -9,16 +9,18 @@ from unittest import mock
 def setup_database():
     ## Set up
     print("\nSet up")
-    db = HelperDB()
+    db = HelperDB(test=True)
     db.set_up_db_connection()
+    print('before the great battle')
+    # Removing all data in the beginning
+    db.cur.execute("TRUNCATE TABLE test_product;")
+    print('I can run this')
 
-    db.cur.execute(
-        f"TRUNCATE TABLE test_product;"
-    )
-    
+    # Adding data
     db.cur.execute(
         f"INSERT INTO test_product(name, price) VALUES('KitKat', 1.2), ('Coke Zero', 1.4);"
     )
+    
     db.disconnect_database()
     
     yield 
@@ -26,17 +28,15 @@ def setup_database():
     print("\nTear down")
     if db.conn.open == False:
         db.set_up_db_connection()
-        
-    db.cur.execute(
-        f"TRUNCATE TABLE test_product;"
-    )
+
+    db.cur.execute("TRUNCATE TABLE test_product;")
     db.disconnect_database()
 
 def test_setup(setup_database):
     ## Intial setup check 
     setup_database
 
-    db = HelperDB()
+    db = HelperDB(test=True)
     actual = db.fetch_all('SELECT * FROM test_product')
     print(actual)
     expected = ((1, 'KitKat', 1.2), (2, 'Coke Zero', 1.4))
@@ -44,45 +44,68 @@ def test_setup(setup_database):
     assert expected == actual
 
 
-def test_show_db_data(setup_database):
-    setup_database
-    actual = show_db_data('test_product')
-    expected = ((1, 'KitKat', 1.2), (2, 'Coke Zero', 1.4))
+def test_show_db_data():
+    actual = show_db_data('status')
+    expected = [{'id': 1, 'status': 'preparing'}, {'id': 2, 'status': 'out-for-delivery'}, {'id': 3, 'status': 'delivered'}]
     
     assert expected == actual
 
-def test_add_to_db(setup_database):
-    setup_database
-    add_to_db('test_product', 'Tomato', 1.2)
-    db = HelperDB()
-    actual = db.fetch_all(f"SELECT * FROM test_product WHERE name = 'Tomato'")[0]
-    
-    assert 'Tomato' and 1.2 in actual
+def test_add_to_db():
+    # Test adding to product
+    add_to_db('product', ['Tomato', 1.2], test=True)
+    db = HelperDB(test=True)
+    actual = db.fetch_all(f"SELECT * FROM product WHERE name = 'Tomato'", dict_cur=True)
+    print(actual)
+    assert ('Tomato' and 1.2 in actual[0].values())
 
-@patch("builtins.input", side_effect=['2', 'Pepsi', ''])
-@patch("builtins.print")
-def test_update_to_db(mock_print, mock_input, setup_database):
-    setup_database
-    mock_input
-    update_to_db('test_product')
-    
-    ## Check if data has gone through database execution
-    mock_print.assert_called_with("\nData has been updated!\n")
-    assert mock_input.call_count == 3
-    
-    ## Check if the data has been changed
-    db = HelperDB()
-    actual = db.fetch_all(f'SELECT * FROM test_product WHERE name = "Pepsi"')[0]
-    assert 'Pepsi' in actual
+    db.execute_operation('DELETE FROM product WHERE name = "Tomato"')
 
-@patch("builtins.print")
-def test_delete_to_db(mock_print, setup_database):
-    setup_database
+    # Test adding to courier
+    add_to_db('courier', ['DPD', '07888665654'], test=True)
+    db = HelperDB(test=True)
+    actual = db.fetch_all(f"SELECT * FROM courier WHERE name = 'DPD'", dict_cur=True)
+    print(actual)
+    assert ('DPD' and '07888665654' in actual[0].values())
+
+    db.execute_operation('DELETE FROM courier WHERE name = "DPD"')
+
+    # Test adding to orders
+    add_to_db('orders', ['Miles', '99th Street', '07872817281',1, 1, [1,2]], test=True)
+    db = HelperDB(test=True)
+
+    actual = db.fetch_all(f"SELECT * FROM orders" 
+        + " INNER JOIN customer_detail"
+        + " ON orders.customer_detail_id = customer_detail_id"
+        + " WHERE customer_name = 'Miles';", dict_cur=True)
+    print(actual)
+    assert ('Miles' and '99th Street' in actual[0].values())
+    db.execute_operation('TRUNCATE TABLE orders;')
+    db.execute_operation('DELETE FROM customer_detail WHERE customer_name = "Miles"')
+
+# @patch("builtins.input", side_effect=['2', 'Pepsi', ''])
+# @patch("builtins.print")
+# def test_update_to_db(mock_print, mock_input, setup_database):
+#     setup_database
+#     mock_input
+#     update_to_db('test_product')
     
-    # Correct selection leading to deletion
-    delete_to_db('test_product', 2)
-    mock_print.assert_called_with('Index entry: 2 has now been deleted!\n')
+#     ## Check if data has gone through database execution
+#     mock_print.assert_called_with("\nData has been updated!\n")
+#     assert mock_input.call_count == 3
     
-    # Wrong selection
-    delete_to_db('sfsafaf', 2)
-    mock_print.assert_called_with("Cannot delete data from the database. Table 'mini_project_storage.sfsafaf' doesn't exist")
+#     ## Check if the data has been changed
+#     db = HelperDB(test=True)
+#     actual = db.fetch_all(f'SELECT * FROM test_product WHERE name = "Pepsi"')[0]
+#     assert 'Pepsi' in actual
+
+# @patch("builtins.print")
+# def test_delete_to_db(mock_print, setup_database):
+#     setup_database
+    
+#     # Correct selection leading to deletion
+#     delete_to_db('test_product', 2)
+#     mock_print.assert_called_with('Index entry: 2 has now been deleted!\n')
+    
+#     # Wrong selection
+#     delete_to_db('sfsafaf', 2)
+#     mock_print.assert_called_with("Cannot delete data from the database. Table 'mini_project_storage.sfsafaf' doesn't exist")
